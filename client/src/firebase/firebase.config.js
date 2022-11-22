0fd03b853fea544d07b1e78317dbe9b8194a14ee
Farvez999 <engrfrzabdullah999@gmail.com>
2022-11-23 00:59:33 +0600

    Sign in and sign up and forget pass done

--- client/src/firebase/firebase.config.js
+++ client/src/firebase/firebase.config.js
@@ -1,8 +1,14 @@
 import { initializeApp } from 'firebase/app'
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  // COnfig code goes here
-}
+  apiKey: "AIzaSyB9DyVRJMocto9SbUW8hWelwIVmQSoHYZk",
+  authDomain: "aircnc-3fa28.firebaseapp.com",
+  projectId: "aircnc-3fa28",
+  storageBucket: "aircnc-3fa28.appspot.com",
+  messagingSenderId: "1055618316683",
+  appId: "1:1055618316683:web:64f69fa3d7966f2302178e"
+};
 
 const app = initializeApp(firebaseConfig)
 
